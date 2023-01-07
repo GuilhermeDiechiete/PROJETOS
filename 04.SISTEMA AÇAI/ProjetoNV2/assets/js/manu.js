9459ba5ff7e112ac34e9eadbda2fc137e0d7c0f2
diechiete2@gmail.com <105297458+GuilhermeDiechiete@users.noreply.github.com>
2023-01-07 14:57:37 -0300

    adicionei sistema acai nv 2

--- 04.SISTEMA AÇAI/ProjetoNV2/assets/js/manu.js
+++ 04.SISTEMA AÇAI/ProjetoNV2/assets/js/manu.js
@@ -5,8 +5,6 @@ const opçãoSabor = document.querySelector('.sabor')
 const botaoEnviar = document.querySelector('.enviar')
 const registroDePedidos = document.querySelector('.registroPedidos')
 
-
-// CAPTURANDO EVENTO DE CLICK NO BOTAO ENVIAR PEDIDO
 botaoEnviar.addEventListener('click', function(){
     criarPedido()})
 
@@ -14,44 +12,36 @@ document.addEventListener('click', function(e){
     const el = e.target
 
     if(el.classList.contains('apagar')){
-        el.parentElement.remove()
-        
-    }
-})
+        el.parentElement.remove()}})
 
 function criarLi(){
     const li = document.createElement('li')
     li.setAttribute('class', 'item')
-    return li
-}
+    return li}
+
 function criarPedido(){
     const li = criarLi()
     li.innerText = concatenarPedido()
     registroDePedidos.appendChild(li)
-    criarBotaoApagar(li)
-    
-    
-}
+    criarBotaoApagar(li)}
+
 function concatenarPedido(){
     const pedido = `MESA: ${opçãoMesa.value} 
                     COPO: ${opçãoTamanho.value} 
                     ADD: ${opçãoSabor.value} `
-    return pedido
-}
+    return pedido}
 
 function criarBotaoApagar(li){
     li.innerText += ' ';
     const botaoApagar = document.createElement('button')
     botaoApagar.innerText = 'Apagar';
     botaoApagar.setAttribute('class', 'apagar')
-    li.appendChild(botaoApagar)
-}
+    li.appendChild(botaoApagar)}
 
 function salvarPedido(){
     const pedidoSalvo = criarPedido()
     localStorage.getItem = pedidoSalvo
-    return pedidoSalvo
-}
+    return pedidoSalvo}
 
 salvarPedido()
 
